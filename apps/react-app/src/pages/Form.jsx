41763bafaefaf71d620a59b1bbe9885854522618
Giulia Ghisini <giulia.ghisini@redturtle.it>
2025-08-29 12:27:51 +0200

    testing react and react docs

---
+++ apps/react-app/src/pages/Form.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Form = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(document.getElementById('form'));
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
+    // Handle form submission logic here
+  };
+
+  return (
+    <form id='form' onSubmit={handleSubmit}>
+      <it-input id='nome' name='nome' label='Nome'></it-input>
+      <it-input id='cognome' name='cognome' label='Cogome'></it-input>
+      <it-input
+        id='email'
+        name='email'
+        label='E-mail'
+        type='email'
+        support-text='Inserisci la tua email'
+        custom-validation
+      ></it-input>
+      <it-input
+        id='messaggio'
+        name='messaggio'
+        label='Messaggio'
+        type='textarea'
+        support-text='Inserisci il tuo messaggio'
+        custom-validation
+      ></it-input>
+
+      <it-button type='submit' variant='primary' block>
+        Invia
+      </it-button>
+    </form>
+  );
+};
+
+export default Form;
--- apps/react-app/src/pages/Icon.jsx
+++ apps/react-app/src/pages/Icon.jsx
@@ -4,213 +4,124 @@ const Icon = () => {
   return (
     <>
       <h1>it-icon</h1>
-      <it-icon name="it-star-full" align="middle" color="inverse"></it-icon>
+      <it-icon name='it-star-full' align='middle'></it-icon>
       <section>
         <h2>Varianti di colore</h2>
-        <div className="flex">
-          <it-icon
-            name="it-star-full"
-            label="col primary"
-            color="primary"
-          ></it-icon>
+        <div className='flex'>
+          <it-icon name='it-star-full' label='col primary' color='primary'></it-icon>
 
-          <it-icon
-            name="it-star-full"
-            label="col secondary"
-            color="secondary"
-          ></it-icon>
+          <it-icon name='it-star-full' label='col secondary' color='secondary'></it-icon>
 
-          <it-icon
-            name="it-star-full"
-            label="col success"
-            color="success"
-          ></it-icon>
+          <it-icon name='it-star-full' label='col success' color='success'></it-icon>
 
-          <it-icon
-            name="it-star-full"
-            label="col warning"
-            color="warning"
-          ></it-icon>
+          <it-icon name='it-star-full' label='col warning' color='warning'></it-icon>
 
-          <it-icon
-            name="it-star-full"
-            label="col danger"
-            color="danger"
-          ></it-icon>
+          <it-icon name='it-star-full' label='col danger' color='danger'></it-icon>
 
-          <it-icon
-            name="it-star-full"
-            label="col inverse"
-            color="inverse"
-          ></it-icon>
+          <div style={{ backgroundColor: '#000' }}>
+            <it-icon name='it-star-full' label='col inverse' color='inverse'></it-icon>
+          </div>
 
-          <it-icon
-            name="it-star-full"
-            label="col light"
-            color="light"
-          ></it-icon>
+          <div style={{ backgroundColor: '#000' }}>
+            <it-icon name='it-star-full' label='col light' color='light'></it-icon>
+          </div>
 
-          <it-icon
-            name="it-star-full"
-            label="col disabled"
-            color="disabled"
-          ></it-icon>
+          <it-icon name='it-star-full' label='col disabled' color='disabled'></it-icon>
 
-          <it-icon
-            name="it-star-full"
-            label="col white"
-            color="white"
-          ></it-icon>
+          <div style={{ backgroundColor: '#000' }}>
+            <it-icon name='it-star-full' label='col white' color='white'></it-icon>
+          </div>
 
-          <span class="bg-light">
-            <it-icon name="it-star-full" label="bg-light"></it-icon>
+          <span class='bg-light'>
+            <it-icon name='it-star-full' label='bg-light'></it-icon>
           </span>
-          <span class="bg-dark">
-            <it-icon name="it-star-full" label="bg-dark"></it-icon>
+          <span class='bg-dark'>
+            <it-icon name='it-star-full' label='bg-dark'></it-icon>
           </span>
         </div>
       </section>
       <section>
         <h2>Varianti di dimensione </h2>
         <div>
-          <div className="flex">
+          <div className='flex'>
             <div>
-              <it-icon
-                name="it-star-full"
-                size="xs"
-                align="middle"
-                color="inverse"
-              ></it-icon>
+              <it-icon name='it-star-full' size='xs' align='middle'></it-icon>
             </div>
             <div>
-              <it-icon
-                name="it-star-full"
-                size="sm"
-                align="middle"
-                color="inverse"
-              ></it-icon>
+              <it-icon name='it-star-full' size='sm' align='middle'></it-icon>
             </div>
             <div>
-              <it-icon
-                name="it-star-full"
-                align="middle"
-                color="inverse"
-              ></it-icon>
+              <it-icon name='it-star-full' align='middle'></it-icon>
             </div>
             <div>
-              <it-icon
-                name="it-star-full"
-                size="lg"
-                align="middle"
-                color="inverse"
-              ></it-icon>
+              <it-icon name='it-star-full' size='lg' align='middle'></it-icon>
             </div>
             <div>
-              <it-icon
-                name="it-star-full"
-                size="xl"
-                align="middle"
-                color="inverse"
-              ></it-icon>
+              <it-icon name='it-star-full' size='xl' align='middle'></it-icon>
             </div>
           </div>
         </div>
       </section>
       <section>
         <h2>Varianti di allineamento</h2>
-        <div className="flex">
+        <div className='flex'>
           <div>
-            <it-icon
-              name="it-star-full"
-              size="lg"
-              label="align top"
-              align="top"
-            ></it-icon>
+            <it-icon name='it-star-full' size='lg' label='align top' align='top'></it-icon>
 
-            <it-icon
-              name="it-star-full"
-              size="lg"
-              label="align middle"
-              align="middle"
-            ></it-icon>
+            <it-icon name='it-star-full' size='lg' label='align middle' align='middle'></it-icon>
 
-            <it-icon
-              name="it-star-full"
-              size="lg"
-              label="align bottom"
-              align="bottom"
-            ></it-icon>
+            <it-icon name='it-star-full' size='lg' label='align bottom' align='bottom'></it-icon>
           </div>
         </div>
       </section>
       <section>
         <h2>Spaziatura</h2>
 
-        <div className="flex">
+        <div className='flex'>
           <div>
-            <it-icon
-              name="it-star-full"
-              size="xs"
-              align="middle"
-              padded={true}
-            ></it-icon>
+            <it-icon name='it-star-full' size='xs' align='middle' padded={true}></it-icon>
           </div>
           <div>
-            <it-icon
-              name="it-star-full"
-              size="sm"
-              align="middle"
-              padded={true}
-            ></it-icon>
+            <it-icon name='it-star-full' size='sm' align='middle' padded={true}></it-icon>
           </div>
           <div>
-            <it-icon name="it-star-full" align="middle" padded={true}></it-icon>
+            <it-icon name='it-star-full' align='middle' padded={true}></it-icon>
           </div>
           <div>
-            <it-icon
-              name="it-star-full"
-              size="lg"
-              align="middle"
-              padded={true}
-            ></it-icon>
+            <it-icon name='it-star-full' size='lg' align='middle' padded={true}></it-icon>
           </div>
           <div>
-            <it-icon
-              name="it-star-full"
-              size="xl"
-              align="middle"
-              padded={true}
-            ></it-icon>
+            <it-icon name='it-star-full' size='xl' align='middle' padded={true}></it-icon>
           </div>
         </div>
       </section>
       <section>
         <h2>SVG personalizzato</h2>
-        <it-icon size="sm" label="Icona utente">
-          <svg viewBox="0 0 24 24">
-            <circle cx="12" cy="12" r="10" fill="red"></circle>
+        <it-icon size='sm' label='Icona utente'>
+          <svg viewBox='0 0 24 24'>
+            <circle cx='12' cy='12' r='10' fill='red'></circle>
           </svg>
         </it-icon>
       </section>
       <section>
         <h2>SVG esterno</h2>
-        <div className="flex">
+        <div className='flex'>
           <it-icon
-            size="xl"
-            label="Stemma 1"
-            src="https://upload.wikimedia.org/wikipedia/commons/1/12/Palermo-Stemma_%281999%29.svg"
+            size='xl'
+            label='Stemma 1'
+            src='https://upload.wikimedia.org/wikipedia/commons/1/12/Palermo-Stemma_%281999%29.svg'
           ></it-icon>
 
           <it-icon
-            size="xl"
-            label="Stemma 2"
-            src="https://upload.wikimedia.org/wikipedia/commons/3/31/Roma-Stemma-2.svg"
+            size='xl'
+            label='Stemma 2'
+            src='https://upload.wikimedia.org/wikipedia/commons/3/31/Roma-Stemma-2.svg'
           ></it-icon>
 
           <it-icon
-            size="xl"
-            label="Stemma 3"
-            src="https://upload.wikimedia.org/wikipedia/commons/9/93/CoA_Citt%C3%A0_di_Milano.svg"
+            size='xl'
+            label='Stemma 3'
+            src='https://upload.wikimedia.org/wikipedia/commons/9/93/CoA_Citt%C3%A0_di_Milano.svg'
           ></it-icon>
         </div>
       </section>
