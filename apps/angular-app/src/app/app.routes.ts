0cde0811374a0869dc487ff8c34f7941d9ebb477
Giulia Ghisini <giulia.ghisini@redturtle.it>
2025-08-29 15:19:53 +0200

    tests

--- apps/angular-app/src/app/app.routes.ts
+++ apps/angular-app/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'button',
+    loadComponent: () => import('./pages/button.component').then((c) => c.ButtonComponent),
+  },
+  // {
+  //   path: 'form',
+  //   loadComponent: () => import('./pages/form.component').then((c) => c.FormComponent),
+  // },
+  // {
+  //   path: 'icon',
+  //   loadComponent: () => import('./pages/icon.component').then((c) => c.IconComponent),
+  // },
+  // {
+  //   path: 'input',
+  //   loadComponent: () => import('./pages/input.component').then((c) => c.InputComponent),
+  // },
+  // {
+  //   path: 'video',
+  //   loadComponent: () => import('./pages/video.component').then((c) => c.VideoComponent),
+  // },
+];
