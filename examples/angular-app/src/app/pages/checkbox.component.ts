c28cc6aa8eb4eddac053948e8b8e63d2bd5137bb
Giulia Ghisini <giulia.ghisini@redturtle.it>
2025-10-02 12:08:35 +0200

    test svelte, vue, react, angular

---
+++ examples/angular-app/src/app/pages/checkbox.component.ts
@@ -0,0 +1,9 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'checkbox-page',
+  templateUrl: './checkbox.component.html',
+  standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class CheckboxComponent {}
