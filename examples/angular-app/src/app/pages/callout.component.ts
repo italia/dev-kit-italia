113188a091f15d543e4ad5ddefe3b878431bf54c
Martina Bustacchini <martina.bustacchini@redturtle.it>
2025-11-04 10:37:59 +0100

    chore: fix angular example

--- examples/angular-app/src/app/pages/callout.component.ts
+++ examples/angular-app/src/app/pages/callout.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-callout',
   standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   template: `
     <h1>it-callout</h1>
     <it-callout>
       <it-icon slot="icon" name="it-info-circle" size="sm" aria-hidden="true"></it-icon>
       <span slot="title">Titolo callout</span>
-      <p>Maecenas vulputate ante dictum vestibulum volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non augue non purus vestibulum varius.</p>
+      <p>
+        Maecenas vulputate ante dictum vestibulum volutpat. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Aenean non augue non purus vestibulum varius.
+      </p>
     </it-callout>
 
     <section>
@@ -18,7 +22,10 @@ import { Component } from '@angular/core';
           <it-callout>
             <it-icon slot="icon" name="it-info-circle" size="sm" aria-hidden="true"></it-icon>
             <span slot="title">Titolo callout</span>
-            <p>Maecenas vulputate ante dictum vestibulum volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non augue non purus vestibulum varius.</p>
+            <p>
+              Maecenas vulputate ante dictum vestibulum volutpat. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Aenean non augue non purus vestibulum varius.
+            </p>
           </it-callout>
         </div>
       </div>
@@ -31,7 +38,10 @@ import { Component } from '@angular/core';
           <it-callout variant="primary">
             <it-icon slot="icon" name="it-info-circle" size="sm" aria-hidden="true"></it-icon>
             <span slot="title">Note a riguardo</span>
-            <p>Quisque ex eros, pellentesque vitae enim sed, pharetra tempus dolor. Donec eu nibh ac lacus luctus pellentesque. Duis interdum scelerisque magna nec malesuada.</p>
+            <p>
+              Quisque ex eros, pellentesque vitae enim sed, pharetra tempus dolor. Donec eu nibh ac
+              lacus luctus pellentesque. Duis interdum scelerisque magna nec malesuada.
+            </p>
           </it-callout>
         </div>
       </div>
@@ -44,7 +54,10 @@ import { Component } from '@angular/core';
           <it-callout variant="success">
             <it-icon slot="icon" name="it-check-circle" size="sm" aria-hidden="true"></it-icon>
             <span slot="title">Titolo di conferma</span>
-            <p>Maecenas vulputate ante dictum vestibulum volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non augue non purus vestibulum varius.</p>
+            <p>
+              Maecenas vulputate ante dictum vestibulum volutpat. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Aenean non augue non purus vestibulum varius.
+            </p>
           </it-callout>
         </div>
       </div>
@@ -57,7 +70,10 @@ import { Component } from '@angular/core';
           <it-callout variant="warning">
             <it-icon slot="icon" name="it-warning-circle" size="sm" aria-hidden="true"></it-icon>
             <span slot="title">Titolo di attenzione</span>
-            <p>Quisque ex eros, pellentesque vitae enim sed, pharetra tempus dolor. Donec eu nibh ac lacus luctus pellentesque. Duis interdum scelerisque magna nec malesuada.</p>
+            <p>
+              Quisque ex eros, pellentesque vitae enim sed, pharetra tempus dolor. Donec eu nibh ac
+              lacus luctus pellentesque. Duis interdum scelerisque magna nec malesuada.
+            </p>
           </it-callout>
         </div>
       </div>
@@ -70,7 +86,10 @@ import { Component } from '@angular/core';
           <it-callout variant="danger">
             <it-icon slot="icon" name="it-close-circle" size="sm" aria-hidden="true"></it-icon>
             <span slot="title">Titolo di allerta</span>
-            <p>Quisque ex eros, pellentesque vitae enim sed, pharetra tempus dolor. Donec eu nibh ac lacus luctus pellentesque. Duis interdum scelerisque magna nec malesuada.</p>
+            <p>
+              Quisque ex eros, pellentesque vitae enim sed, pharetra tempus dolor. Donec eu nibh ac
+              lacus luctus pellentesque. Duis interdum scelerisque magna nec malesuada.
+            </p>
           </it-callout>
         </div>
       </div>
@@ -82,7 +101,11 @@ import { Component } from '@angular/core';
         <div class="col-12 col-lg-6">
           <it-callout highlight>
             <span slot="title">Titolo callout</span>
-            <p>Maecenas vulputate ante dictum vestibulum volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non augue non purus vestibulum varius. Maecenas ullamcorper tincidunt nulla quis laoreet.</p>
+            <p>
+              Maecenas vulputate ante dictum vestibulum volutpat. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Aenean non augue non purus vestibulum varius. Maecenas
+              ullamcorper tincidunt nulla quis laoreet.
+            </p>
           </it-callout>
         </div>
       </div>
@@ -94,7 +117,11 @@ import { Component } from '@angular/core';
         <div class="col-12 col-lg-6">
           <it-callout variant="primary" highlight>
             <span slot="title">Titolo callout</span>
-            <p>Maecenas vulputate ante dictum vestibulum volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non augue non purus vestibulum varius. Maecenas ullamcorper tincidunt nulla quis laoreet.</p>
+            <p>
+              Maecenas vulputate ante dictum vestibulum volutpat. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Aenean non augue non purus vestibulum varius. Maecenas
+              ullamcorper tincidunt nulla quis laoreet.
+            </p>
           </it-callout>
         </div>
       </div>
@@ -107,7 +134,14 @@ import { Component } from '@angular/core';
           <it-callout variant="success" highlight>
             <it-icon slot="icon" name="it-check-circle" size="sm" aria-hidden="true"></it-icon>
             <span slot="title">Titolo di conferma</span>
-            <p>Maecenas at erat id sem interdum efficitur eu sed nunc. Mauris sit amet erat eget augue molestie malesuada ut sed ex. In sed dignissim elit. Donec efficitur, sem eget vestibulum auctor, sem erat interdum magna, eu commodo odio mauris semper dolor. Maecenas vulputate ante dictum vestibulum volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non augue non purus vestibulum varius. Maecenas ullamcorper tincidunt nulla quis laoreet.</p>
+            <p>
+              Maecenas at erat id sem interdum efficitur eu sed nunc. Mauris sit amet erat eget
+              augue molestie malesuada ut sed ex. In sed dignissim elit. Donec efficitur, sem eget
+              vestibulum auctor, sem erat interdum magna, eu commodo odio mauris semper dolor.
+              Maecenas vulputate ante dictum vestibulum volutpat. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Aenean non augue non purus vestibulum varius. Maecenas
+              ullamcorper tincidunt nulla quis laoreet.
+            </p>
           </it-callout>
         </div>
       </div>
@@ -120,7 +154,14 @@ import { Component } from '@angular/core';
           <it-callout variant="warning" highlight>
             <it-icon slot="icon" name="it-warning-circle" size="sm" aria-hidden="true"></it-icon>
             <span slot="title">Titolo di attenzione</span>
-            <p>Maecenas at erat id sem interdum efficitur eu sed nunc. Mauris sit amet erat eget augue molestie malesuada ut sed ex. In sed dignissim elit. Donec efficitur, sem eget vestibulum auctor, sem erat interdum magna, eu commodo odio mauris semper dolor. Maecenas vulputate ante dictum vestibulum volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non augue non purus vestibulum varius. Maecenas ullamcorper tincidunt nulla quis laoreet.</p>
+            <p>
+              Maecenas at erat id sem interdum efficitur eu sed nunc. Mauris sit amet erat eget
+              augue molestie malesuada ut sed ex. In sed dignissim elit. Donec efficitur, sem eget
+              vestibulum auctor, sem erat interdum magna, eu commodo odio mauris semper dolor.
+              Maecenas vulputate ante dictum vestibulum volutpat. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Aenean non augue non purus vestibulum varius. Maecenas
+              ullamcorper tincidunt nulla quis laoreet.
+            </p>
           </it-callout>
         </div>
       </div>
@@ -133,7 +174,14 @@ import { Component } from '@angular/core';
           <it-callout variant="danger" highlight>
             <it-icon slot="icon" name="it-close-circle" size="sm" aria-hidden="true"></it-icon>
             <span slot="title">Titolo di allerta</span>
-            <p>Maecenas at erat id sem interdum efficitur eu sed nunc. Mauris sit amet erat eget augue molestie malesuada ut sed ex. In sed dignissim elit. Donec efficitur, sem eget vestibulum auctor, sem erat interdum magna, eu commodo odio mauris semper dolor. Maecenas vulputate ante dictum vestibulum volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non augue non purus vestibulum varius. Maecenas ullamcorper tincidunt nulla quis laoreet.</p>
+            <p>
+              Maecenas at erat id sem interdum efficitur eu sed nunc. Mauris sit amet erat eget
+              augue molestie malesuada ut sed ex. In sed dignissim elit. Donec efficitur, sem eget
+              vestibulum auctor, sem erat interdum magna, eu commodo odio mauris semper dolor.
+              Maecenas vulputate ante dictum vestibulum volutpat. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Aenean non augue non purus vestibulum varius. Maecenas
+              ullamcorper tincidunt nulla quis laoreet.
+            </p>
           </it-callout>
         </div>
       </div>
@@ -145,12 +193,32 @@ import { Component } from '@angular/core';
         <div class="col-12">
           <it-callout more>
             <span slot="title">Approfondimento</span>
-            <p>Quisque suscipit interdum augue non volutpat. Cras tristique arcu tortor. Mauris eu magna nibh. Curabitur malesuada neque in lectus sagittis accumsan. In vitae justo eros. Maecenas pellentesque lacinia ipsum vitae rhoncus. Vestibulum pretium tempor turpis, nec gravida eros viverra in. Proin dictum nibh ut semper tristique.</p>
-            <p>Maecenas at erat id sem interdum efficitur eu sed nunc. Mauris sit amet erat eget augue molestie malesuada ut sed ex. In sed dignissim elit. Donec efficitur, sem eget vestibulum auctor, sem erat interdum magna, eu commodo odio mauris semper dolor.</p>
-            <p>Maecenas vulputate ante dictum vestibulum volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non augue non purus vestibulum varius. Maecenas ullamcorper tincidunt nulla quis laoreet.</p>
+            <p>
+              Quisque suscipit interdum augue non volutpat. Cras tristique arcu tortor. Mauris eu
+              magna nibh. Curabitur malesuada neque in lectus sagittis accumsan. In vitae justo
+              eros. Maecenas pellentesque lacinia ipsum vitae rhoncus. Vestibulum pretium tempor
+              turpis, nec gravida eros viverra in. Proin dictum nibh ut semper tristique.
+            </p>
+            <p>
+              Maecenas at erat id sem interdum efficitur eu sed nunc. Mauris sit amet erat eget
+              augue molestie malesuada ut sed ex. In sed dignissim elit. Donec efficitur, sem eget
+              vestibulum auctor, sem erat interdum magna, eu commodo odio mauris semper dolor.
+            </p>
+            <p>
+              Maecenas vulputate ante dictum vestibulum volutpat. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Aenean non augue non purus vestibulum varius. Maecenas
+              ullamcorper tincidunt nulla quis laoreet.
+            </p>
             <div slot="more-content">
               <div class="collapse-header d-flex align-items-center justify-content-between">
-                <button class="btn btn-outline-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                <button
+                  class="btn btn-outline-primary"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseExample"
+                  aria-expanded="false"
+                  aria-controls="collapseExample"
+                >
                   Leggi tutto
                 </button>
                 <a href="#" class="btn btn-primary ms-2">
@@ -160,7 +228,10 @@ import { Component } from '@angular/core';
               </div>
               <div class="collapse" id="collapseExample">
                 <div class="collapse-content">
-                  <p>Contenuto aggiuntivo che viene mostrato quando il collapse è espanso. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  <p>
+                    Contenuto aggiuntivo che viene mostrato quando il collapse è espanso. Lorem
+                    ipsum dolor sit amet, consectetur adipiscing elit.
+                  </p>
                 </div>
               </div>
             </div>
