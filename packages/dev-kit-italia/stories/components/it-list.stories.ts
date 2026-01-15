c421ea760a80b7c843aa6b7ed4660313a168daef
Piero Nicolli <piero.nicolli@redturtle.it>
2026-01-15 09:47:49 +0100

    docs: updated docs tag

--- packages/dev-kit-italia/stories/components/it-list.stories.ts
+++ packages/dev-kit-italia/stories/components/it-list.stories.ts
@@ -7,7 +7,7 @@ import '@italia/checkbox';
 
 const meta = {
   title: 'Componenti/List',
-  tags: ['new', 'a11y-ok', 'documentation'],
+  tags: ['beta', 'a11y-ok', 'documentation'],
   parameters: {
     layout: 'padded',
   },
