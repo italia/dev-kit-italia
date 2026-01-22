fd3c36d2e51cc649443d54b3494122cec4c775b6
Giulia Ghisini <51911425+giuliaghisini@users.noreply.github.com>
2026-01-22 14:29:15 +0100

    Update packages/navscroll/src/types.ts
    Co-authored-by: Piero Nicolli <pnicolli@users.noreply.github.com>

--- packages/navscroll/src/types.ts
+++ packages/navscroll/src/types.ts
@@ -1,6 +1,6 @@
-export const POSITION = ['top', 'bottom'];
-export const LINE_POSITION = [null, 'left', 'right'];
-export const DARK_MODE = [null, 'mobile', 'desktop', 'always'];
+export const POSITION = ['top', 'bottom'] as const;
+export const LINE_POSITION = [null, 'left', 'right'] as const;
+export const DARK_MODE = [null, 'mobile', 'desktop', 'always'] as const;
 
 export type Position = (typeof POSITION)[number];
 export type LinePosition = (typeof LINE_POSITION)[number];
