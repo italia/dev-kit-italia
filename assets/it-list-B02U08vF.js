e79fac0976a52468f833a430ec10d3bbcef409b6
giuliaghisini <giuliaghisini@users.noreply.github.com>
2026-03-02 14:49:00 +0000

    chore: update documentation [ci skip]

---
+++ assets/it-list-B02U08vF.js
@@ -0,0 +1,108 @@
+import{j as i,M as t,T as l,C as n}from"./blocks-CAFt6Kk-.js";import{useMDXComponents as o}from"./index-DsmfhIK1.js";import{L as c,a as r,b as d,c as h,d as p,e as x,f as j,g as m,h as g,i as u,j as f,k as L,l as z,m as v,n as k,o as b,p as C,q as I,r as P,s as w,t as A,u as M,v as D}from"./it-list.stories-CvZeaS9S.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-CLk7YE6Q.js";function s(a){const e={a:"a",code:"code",h2:"h2",h3:"h3",h4:"h4",h5:"h5",li:"li",p:"p",ul:"ul",...o(),...a.components};return i.jsxs(i.Fragment,{children:[i.jsx(t,{of:c}),`
+`,i.jsx(l,{}),`
+`,i.jsx("description",{children:"Lista di testi, link e altri elementi per presentare contenuti correlati o facilitare la navigazione."}),`
+`,i.jsx(e.h2,{id:"cosa-fa",children:"Cosa fa"}),`
+`,i.jsx(e.p,{children:"Il componente List, in italiano lista, raggruppa in una lista verticale una serie di contenuti correlati."}),`
+`,i.jsx(e.p,{children:i.jsx(e.a,{href:"https://designers.italia.it/design-system/componenti/list/",rel:"nofollow",children:"Approfondisci come e quando usare il componente List"})}),`
+`,i.jsx(e.h3,{id:"alternative-a-questo-componente",children:"Alternative a questo componente"}),`
+`,i.jsxs(e.ul,{children:[`
+`,i.jsx(e.li,{children:"Tables (in fase di sviluppo): utili a mostrare set di dati più complessi."}),`
+`,i.jsxs(e.li,{children:[i.jsx(e.a,{href:"/docs/componenti-dropdown--documentazione",children:"Dropdown"}),": utili per creare menu contestuali di navigazione laddove le scelte siano limitate."]}),`
+`]}),`
+`,i.jsx(e.h2,{id:"anteprima-del-componente",children:"Anteprima del componente"}),`
+`,i.jsx(n,{of:r}),`
+`,i.jsx(e.h2,{id:"tipologie-di-lista",children:"Tipologie di lista"}),`
+`,i.jsxs(e.p,{children:["Le liste, costituite da tag ",i.jsx(e.code,{children:"<ul>"})," con classe ",i.jsx(e.code,{children:".it-list"})," all'interno di un wrapper con classe ",i.jsx(e.code,{children:".it-list-wrapper"}),", possono contenere testi, link, icone, avatar, immagini o una combinazione di questi elementi."]}),`
+`,i.jsx(e.h3,{id:"lista-con-avatar",children:"Lista con avatar"}),`
+`,i.jsxs(e.p,{children:["Per aggiungere un avatar, inserisci l'elemento ",i.jsx(e.code,{children:"it-avatar"})," prima dell'elemento ",i.jsx(e.code,{children:".it-right-zone"})," che contiene il testo."]}),`
+`,i.jsx(n,{of:d}),`
+`,i.jsx(e.h3,{id:"lista-con-icona",children:"Lista con icona"}),`
+`,i.jsxs(e.p,{children:["Per aggiungere un'icona, inserisci l'elemento ",i.jsx(e.code,{children:".it-rounded-icon"})," con all'interno la relativa icona prima dell'elemento ",i.jsx(e.code,{children:".it-right-zone"})," che contiene il testo."]}),`
+`,i.jsx(n,{of:h}),`
+`,i.jsx(e.h3,{id:"lista-con-immagine",children:"Lista con immagine"}),`
+`,i.jsxs(e.p,{children:["Per aggiungere un'immagine, inserisci l'elemento ",i.jsx(e.code,{children:".it-thumb"})," con all'interno la relativa immagine prima dell'elemento ",i.jsx(e.code,{children:".it-right-zone"}),` che contiene il testo.
+L'immagine ha una dimensione massima di `,i.jsx(e.code,{children:"5rem"}),", che corrispondono a ",i.jsx(e.code,{children:"80px"}),`.
+Puoi modificare le proporzioni dell'immagine usando utilizzando le `,i.jsx(e.a,{href:"https://designers.italia.it/design-system/fondamenti/proporzioni-media/",rel:"nofollow",children:"classi specifiche per le proporzioni"}),"."]}),`
+`,i.jsx(n,{of:p}),`
+`,i.jsx(e.h2,{id:"lista-con-azioni",children:"Lista con azioni"}),`
+`,i.jsx(e.p,{children:"Le liste con azioni presentano icone o elementi interattivi associati all'azione stessa."}),`
+`,i.jsx(e.h3,{id:"con-freccia",children:"Con freccia"}),`
+`,i.jsxs(e.p,{children:["Per aggiungere una freccia, inserisci l'elemento ",i.jsx(e.code,{children:"it-icon"})," dopo l'elemento ",i.jsx(e.code,{children:".text"})," che contiene il testo."]}),`
+`,i.jsx(n,{of:x}),`
+`,i.jsx(e.h3,{id:"con-azioni-multiple",children:"Con azioni multiple"}),`
+`,i.jsxs(e.p,{children:["Per aggiungere più azioni, inserisci l'elemento ",i.jsx(e.code,{children:".it-multiple"})," con all'interno le relative icone dopo l'elemento ",i.jsx(e.code,{children:".text"}),"."]}),`
+`,i.jsx(n,{of:j}),`
+`,i.jsx(e.h2,{id:"altre-varianti",children:"Altre varianti"}),`
+`,i.jsx(e.h3,{id:"con-metadata",children:"Con metadata"}),`
+`,i.jsxs(e.p,{children:["Per aggiungere un campo testuale di metadati, inserisci l'elemento ",i.jsx(e.code,{children:".metadata"})," dopo l'elemento ",i.jsx(e.code,{children:".text"}),"."]}),`
+`,i.jsx(n,{of:m}),`
+`,i.jsx(e.h3,{id:"con-testo-aggiuntivo-azioni-multiple-e-metadata",children:"Con testo aggiuntivo, azioni multiple e metadata"}),`
+`,i.jsxs(e.p,{children:["Per aggiungere testo aggiuntivo ad ogni elemento della lista, usa titolo (heading) e paragrafo ",i.jsx(e.code,{children:"<p>"}),", mantenendo la classe ",i.jsx(e.code,{children:".text"}),` per l'elemento titolo.
+Includi entrambi in un elemento `,i.jsx(e.code,{children:"<div>"})," per allinearli correttamente."]}),`
+`,i.jsx(n,{of:g}),`
+`,i.jsx(e.h2,{id:"liste-per-menu-di-navigazione",children:"Liste per menu di navigazione"}),`
+`,i.jsxs(e.p,{children:["Per creare le voci di un megamenu, una sidebar o una navscroll, usa il tag ",i.jsx(e.code,{children:"<ul>"})," con classe ",i.jsx(e.code,{children:".link-list"})," all'interno di un wrapper con classe ",i.jsx(e.code,{children:".link-list-wrapper"}),"."]}),`
+`,i.jsx(e.h3,{id:"linea-singola",children:"Linea singola"}),`
+`,i.jsx(n,{of:u}),`
+`,i.jsx(e.h4,{id:"elemento-con-stato-attivo",children:"Elemento con stato attivo"}),`
+`,i.jsxs(e.p,{children:["Per attivare un elemento, aggiungi la classe ",i.jsx(e.code,{children:".active"})," al relativo link."]}),`
+`,i.jsx(n,{of:f}),`
+`,i.jsx(e.h4,{id:"elemento-con-stato-disabilitato",children:"Elemento con stato disabilitato"}),`
+`,i.jsxs(e.p,{children:["Per disabilitare un elemento, aggiungi la classe ",i.jsx(e.code,{children:".disabled"})," al relativo link."]}),`
+`,i.jsx(n,{of:L}),`
+`,i.jsx(e.h4,{id:"intestazione-e-divisore",children:"Intestazione e divisore"}),`
+`,i.jsx(e.p,{children:"Puoi aggiungere un'intestazione (con o senza link) e/o divisori per separare gruppi di link."}),`
+`,i.jsxs(e.p,{children:["L'intestazione è costituita dall'elemento ",i.jsx(e.code,{children:".link-list-heading"})," che precede il tag ",i.jsx(e.code,{children:"<ul>"}),`.
+Il divisore è costituito dal tag `,i.jsx(e.code,{children:"<span>"})," con classe ",i.jsx(e.code,{children:".divider"})," e attributo ",i.jsx(e.code,{children:'role="separator"'})," all'interno del tag ",i.jsx(e.code,{children:"<li>"}),"."]}),`
+`,i.jsx(n,{of:z}),`
+`,i.jsx(n,{of:v}),`
+`,i.jsx(e.h3,{id:"dimensioni",children:"Dimensioni"}),`
+`,i.jsxs(e.p,{children:["Per ogni elemento di una lista di link puoi definire una variante di dimensione maggiore usando la classe ",i.jsx(e.code,{children:".large"})," applicata al tag ",i.jsx(e.code,{children:"<a>"}),"."]}),`
+`,i.jsx(n,{of:k}),`
+`,i.jsx(e.h3,{id:"multiline-con-icona",children:"Multiline con icona"}),`
+`,i.jsx(e.p,{children:"Puoi aggiungere un'icona (a destra o sinistra del testo) e un abstract per ogni elemento di una lista di link."}),`
+`,i.jsxs(e.p,{children:["Per includere un'icona, aggiungi al tag ",i.jsx(e.code,{children:"<a>"})," una delle seguenti classi:"]}),`
+`,i.jsxs(e.ul,{children:[`
+`,i.jsxs(e.li,{children:[i.jsx(e.code,{children:"icon-right"}),", per posizionare l'icona a destra del testo;"]}),`
+`,i.jsxs(e.li,{children:[i.jsx(e.code,{children:"icon-left"}),", per posizionare l'icona a sinistra del testo."]}),`
+`]}),`
+`,i.jsxs(e.p,{children:["Puoi inserire ",i.jsx(e.a,{href:"/docs/componenti-icon--documentazione",children:"l'icona necessaria"})," all'interno del tag ",i.jsx(e.code,{children:'<span class="list-item-title-icon-wrapper">'}),", subito dopo o prima dello ",i.jsx(e.code,{children:'<span class="list-item-title">'})," contenente il testo."]}),`
+`,i.jsx(n,{of:b}),`
+`,i.jsx(e.h3,{id:"lista-con-controlli",children:"Lista con controlli"}),`
+`,i.jsx(e.p,{children:`Le liste di link con controlli sono caratterizzate da icone a destra e sinistra del testo.
+Quella di destra è descrittiva mentre quella di sinistra può essere un'eventuale azione aggiuntiva da gestire in JavaScript.`}),`
+`,i.jsxs(e.p,{children:["Aggiungi al tag ",i.jsx(e.code,{children:"<a>"})," la classe ",i.jsx(e.code,{children:".icon-right"})," per posizionare l'icona a destra del testo, oppure la classe ",i.jsx(e.code,{children:".icon-left"})," per posizionare l'icona a sinistra del testo."]}),`
+`,i.jsx(e.h4,{id:"azione-primaria",children:"Azione primaria"}),`
+`,i.jsx(e.h5,{id:"icona-a-sinistra",children:"Icona a sinistra"}),`
+`,i.jsxs(e.p,{children:["Inserisci ",i.jsx(e.a,{href:"/docs/componenti-icon--documentazione",children:"l'icona necessaria"})," all'interno del tag ",i.jsx(e.code,{children:'<span class="list-item-title-icon-wrapper">'}),", subito prima del tag ",i.jsx(e.code,{children:'<span class="list-item-title">'}),` contenente il testo.
+Per posizionarla correttamente a sinistra del testo, aggiungi la classe `,i.jsx(e.code,{children:".icon-left"})," al tag ",i.jsx(e.code,{children:"<a>"}),"."]}),`
+`,i.jsx(n,{of:C}),`
+`,i.jsx(e.h4,{id:"azione-secondaria",children:"Azione secondaria"}),`
+`,i.jsx(e.h5,{id:"icona-a-destra",children:"Icona a destra"}),`
+`,i.jsxs(e.p,{children:["Inserisci ",i.jsx(e.a,{href:"/docs/componenti-icon--documentazione",children:"l'icona necessaria"})," all'interno del tag ",i.jsx(e.code,{children:'<span class="list-item-title-icon-wrapper">'}),", subito dopo il tag ",i.jsx(e.code,{children:'<span class="list-item-title">'}),` contenente il testo.
+Per posizionarla correttamente a destra del testo, aggiungi la classe `,i.jsx(e.code,{children:".icon-right"})," al tag ",i.jsx(e.code,{children:"<a>"}),"."]}),`
+`,i.jsx(n,{of:I}),`
+`,i.jsx(e.h4,{id:"azioni-primaria-e-secondaria",children:"Azioni primaria e secondaria"}),`
+`,i.jsx(e.h5,{id:"icona-a-sinistra-1",children:"Icona a sinistra"}),`
+`,i.jsxs(e.p,{children:["Inserisci l'icona all'interno del tag ",i.jsx(e.code,{children:'<span class="list-item-title-icon-wrapper">'}),", subito prima del tag ",i.jsx(e.code,{children:'<span class="list-item-title">'}),` contenente il testo.
+Per posizionarla a sinistra del testo, aggiungi la classe `,i.jsx(e.code,{children:".icon-left"})," al tag ",i.jsx(e.code,{children:"<a>"}),"."]}),`
+`,i.jsx(e.h5,{id:"icona-a-destra-1",children:"Icona a destra"}),`
+`,i.jsxs(e.p,{children:["Inserisci l'icona all'interno del tag ",i.jsx(e.code,{children:'<span class="list-item-title-icon-wrapper">'}),", subito dopo del tag ",i.jsx(e.code,{children:'<span class="list-item-title">'}),` contenente il testo.
+Per posizionarla a destra del testo, aggiungi la classe `,i.jsx(e.code,{children:".icon-right"})," al tag ",i.jsx(e.code,{children:"<a>"}),"."]}),`
+`,i.jsx(n,{of:P}),`
+`,i.jsx(e.h4,{id:"lista-con-toggle",children:"Lista con toggle"}),`
+`,i.jsxs("div",{class:"alert alert-warning",role:"alert",children:[i.jsx("it-icon",{name:"it-warning-circle",color:"warning",class:"me-2"}),i.jsxs("span",{class:"font-sans-serif",children:["Questo caso d'uso è in fase di sviluppo, sarà completo non appena sarà pronto il componente ",i.jsx(e.code,{children:"it-toggle"}),"."]})]}),`
+`,i.jsxs(e.p,{children:["Una lista di link può contenere anche elementi appartenenti ai form, come ad esempio un ",i.jsx(e.a,{href:"/docs/componenti-toggle--documentazione",children:"toggle"}),"."]}),`
+`,i.jsx(n,{of:w}),`
+`,i.jsx(e.h4,{id:"lista-con-checkbox",children:"Lista con checkbox"}),`
+`,i.jsxs(e.p,{children:["Una lista di link può contenere anche elementi appartenenti ai form, come ad esempio una ",i.jsx(e.a,{href:"/docs/componenti-form-checkbox--documentazione",children:"checkbox"}),"."]}),`
+`,i.jsx(n,{of:A}),`
+`,i.jsx(e.h3,{id:"liste-annidate",children:"Liste annidate"}),`
+`,i.jsx(e.p,{children:"Le liste di link possono contenere link con sottosezioni espanse o collassabili."}),`
+`,i.jsx(e.h4,{id:"espansa",children:"Espansa"}),`
+`,i.jsx(e.p,{children:"Esempio di lista annidata espansa."}),`
+`,i.jsx(n,{of:M}),`
+`,i.jsx(e.h4,{id:"collassabile",children:"Collassabile"}),`
+`,i.jsxs(e.p,{children:[`Esempio di lista annidata collassabile.
+Per questo tipo di lista di link sono stati utilizzati, oltre alle classi custom, i componenti `,i.jsx(e.a,{href:"/docs/componenti-collapse--documentazione",children:"Collapse"})," e ",i.jsx(e.a,{href:"/docs/componenti-icon--documentazione",children:"Icon"}),"."]}),`
+`,i.jsx(n,{of:D})]})}function X(a={}){const{wrapper:e}={...o(),...a.components};return e?i.jsx(e,{...a,children:i.jsx(s,{...a})}):s(a)}export{X as default};
