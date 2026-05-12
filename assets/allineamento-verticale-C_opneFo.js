25ce7a1040c15f92255f7ee32ee3a9b5dec18c14
pnicolli <pnicolli@users.noreply.github.com>
2026-05-12 13:19:55 +0000

    chore: update documentation [ci skip]

---
+++ assets/allineamento-verticale-C_opneFo.js
@@ -0,0 +1,9 @@
+import{j as e,M as a,C as i}from"./blocks-D6EPeW41.js";import{useMDXComponents as t}from"./index-DaLkFkQr.js";import{S as r,C as c,a as s}from"./allineamento-verticale.stories-BRS8oGLu.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-taocDA-g.js";import"./organizzare-gli-spazi.helpers-DVnyiL10.js";function o(l){const n={a:"a",code:"code",h1:"h1",p:"p",...t(),...l.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:r}),`
+`,e.jsx(n.h1,{id:"allineamento-verticale",children:"Allineamento verticale"}),`
+`,e.jsxs(n.p,{children:["Cambia l'allineamento verticale, con le utilità di ",e.jsx(n.a,{href:"https://developer.mozilla.org/en-US/docs/Web/CSS/vertical-align",rel:"nofollow",children:"allineamento verticale"}),`
+di elementi inline, inline-block, inline-table e celle di tabella.`]}),`
+`,e.jsxs(n.p,{children:["Puoi scegliere tra ",e.jsx(n.code,{children:".align-baseline"}),", ",e.jsx(n.code,{children:".align-top"}),", ",e.jsx(n.code,{children:".align-middle"}),", ",e.jsx(n.code,{children:".align-bottom"}),", ",e.jsx(n.code,{children:".align-text-bottom"}),", e ",e.jsx(n.code,{children:".align-text-top"}),"."]}),`
+`,e.jsx(n.p,{children:"Con elementi inline:"}),`
+`,e.jsx(i,{of:c}),`
+`,e.jsx(n.p,{children:"Con le celle di tabella:"}),`
+`,e.jsx(i,{of:s})]})}function C(l={}){const{wrapper:n}={...t(),...l.components};return n?e.jsx(n,{...l,children:e.jsx(o,{...l})}):o(l)}export{C as default};
---
+++ assets/dimensionamento-Dyd_mxl_.js
@@ -0,0 +1,12 @@
+import{j as e,M as r,C as i}from"./blocks-D6EPeW41.js";import{useMDXComponents as o}from"./index-DaLkFkQr.js";import{S as d,L as c,A as t,a as h,b as l}from"./dimensionamento.stories-DL35BPps.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-taocDA-g.js";import"./organizzare-gli-spazi.helpers-DVnyiL10.js";function a(n){const s={code:"code",h1:"h1",h2:"h2",p:"p",...o(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:d}),`
+`,e.jsx(s.h1,{id:"dimensionamento",children:"Dimensionamento"}),`
+`,e.jsx(s.h2,{id:"larghezza-fissa",children:"Larghezza fissa"}),`
+`,e.jsxs(s.p,{children:["Le grandezze di default presenti sono: ",e.jsx(s.code,{children:"25%"}),", ",e.jsx(s.code,{children:"50%"}),", ",e.jsx(s.code,{children:"75%"})," e ",e.jsx(s.code,{children:"100%"}),"."]}),`
+`,e.jsx(i,{of:c}),`
+`,e.jsx(s.h2,{id:"altezza-fissa",children:"Altezza fissa"}),`
+`,e.jsxs(s.p,{children:["Le altezze di default presenti sono: ",e.jsx(s.code,{children:"25%"}),", ",e.jsx(s.code,{children:"50%"}),", ",e.jsx(s.code,{children:"75%"})," e ",e.jsx(s.code,{children:"100%"}),"."]}),`
+`,e.jsx(i,{of:t}),`
+`,e.jsx(s.h2,{id:"larghezza-massima",children:"Larghezza massima"}),`
+`,e.jsxs(s.p,{children:["Sono previste anche le classi ",e.jsx(s.code,{children:"mw-100"})," e ",e.jsx(s.code,{children:"mh-100"})," per i corrispettivi ",e.jsx(s.code,{children:"max-width: 100%;"})," e ",e.jsx(s.code,{children:"max-height: 100%;"}),"."]}),`
+`,e.jsx(i,{of:h}),`
+`,e.jsx(i,{of:l})]})}function u(n={}){const{wrapper:s}={...o(),...n.components};return s?e.jsx(s,{...n,children:e.jsx(a,{...n})}):a(n)}export{u as default};
