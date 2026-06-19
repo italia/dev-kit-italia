855df3c81a5c9037d1b706ae123ecafb3eb5b3a3
Martina Bustacchini <41484878+deodorhunter@users.noreply.github.com>
2026-06-19 12:34:57 +0200

    fix: it-carousel review and issues fixes (#423)
    * fix: carousel review fixes
    
    * docs: update carousel a11y storybook documentation
    
    * fix: carousel review fixes
    
    * fix: fix failing test
    
    * chore: changeset
    
    * docs: fix aria-live compatibility table not rendering in carousel MDX

--- examples/vanilla-app/src/main.js
+++ examples/vanilla-app/src/main.js
@@ -37,6 +37,7 @@ const componentsMenuItems = [
   { label: 'Breadcrumbs', href: '/src/breadcrumbs' },
   { label: 'Button', href: '/src/button' },
   { label: 'Callout', href: '/src/callout' },
+  { label: 'Carousel', href: '/src/carousel' },
   { label: 'Card', href: '/src/card' },
   { label: 'Checkbox', href: '/src/checkbox' },
   { label: 'Chip', href: '/src/chip' },
