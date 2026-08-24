fe34e0f34f7f67f999b656d87839701f30fa0c8e
Andrea Stagi <stagi.andrea@gmail.com>
2026-08-24 16:57:19 +0200

    docs: fix logo examples

--- examples/vanilla-app/src/main.js
+++ examples/vanilla-app/src/main.js
@@ -87,9 +87,6 @@ if (appHeader) {
   appHeader.innerHTML = `
   <div class="app-header bg-primary">
     <div class="container">
-      <a href="https://vite.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
-      </a>
       <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
         <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
       </a>
